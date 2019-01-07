6b5488c12feafe248f5606c24ac0db373e6e4287
Niggarilomon <losgamers123456@gmail.com>
2019-01-07 14:45:47 -0300

    14:45 07/01

--- server/src/www/app/controllers/controller.js
+++ server/src/www/app/controllers/controller.js
@@ -7,3 +7,22 @@ app.controller('Users', function($scope, $http, $interval){
         console.log(response);
     });
 });
+
+
+app.controller('register', function($scope, $http){
+    $scope.submit = function(){
+        name = $scope.name;
+        email = $scope.email;
+        password = $scope.password;
+        data = JSON.parse('{ "name": "'+name+'", "email": "'+email+'", "password": "'+password+'"}');
+        
+        $http.post('/auth/register', data)
+        .success(function(response){
+            console.log(response);
+        })
+        .error(function(response){
+            console.log(response);
+        });
+
+    };
+});
