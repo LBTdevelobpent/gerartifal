967e262efc7096e137807291cce1a46fe505e1ed
Niggarilomon <losgamers123456@gmail.com>
2019-01-07 22:14:21 -0300

    22:14 07/01

--- src/www/app/controllers/controller.js
+++ src/www/app/controllers/controller.js
@@ -1,28 +1,25 @@
 var app = angular.module('app', []); 
 
-app.controller('Users', function($scope, $http, $interval){
-    $http.get('/auth/getAll')
-    .success(function(response){
-        $scope.users = response.user;
-        console.log(response);
-    });
-});
-
-
 app.controller('register', function($scope, $http){
     $scope.submit = function(){
         name = $scope.name;
-        email = $scope.email;
         password = $scope.password;
+        email = $scope.email;
         data = JSON.parse('{ "name": "'+name+'", "email": "'+email+'", "password": "'+password+'"}');
-        
-        $http.post('/auth/register', data)
-        .success(function(response){
-            console.log(response);
-        })
-        .error(function(response){
-            console.log(response);
-        });
-
+    
+        if(name=="undefined" || !email || !password){
+            
+        }else if(password.length < 8){
+          document.getElementById("senha-invalida").innerHTML = "senha invalida, a senha deve conter no mínimo 8 dígitos";
+        }
+        else{
+            $http.post('/auth/register', data)
+            .success(function(response){
+                console.log(response);
+            })
+            .error(function(response){
+                alert(response.error);
+            });
+        }
     };
 });
