75bad5c3f8f5cdfacd9dbcfea2040ad956ea8185
Niggarilomon <losgamers123456@gmail.com>
2019-01-12 17:45:38 -0300

    17:45 12/01

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const User = require('./app/models/user.js');
+
 
 //--------------------Rederização da pagina-------//
 
@@ -22,3 +24,27 @@ require("./app/controllers/index.js")(app);
 
 
 app.listen(3000);
+
+//--------------------------Apagar diariamente contas não verificadas------------------
+function sleep(h) {
+    return new Promise(resolve => setTimeout(resolve, h*3600000));
+  }
+
+async function demo() {
+    await sleep(24);
+
+    var users = await User.find({ verified: false })
+    .select("+passwordResetExpires verified");
+    const now = new Date();
+    if(!users){
+        demo();
+    }
+    for(c = 0; c < users.length; c++){
+        if(now > users[c].passwordResetExpires){
+            await User.findByIdAndDelete(users[c]._id);
+        }
+    }
+    demo();
+}
+demo()
+//----------------------------------------------------------------------------------
