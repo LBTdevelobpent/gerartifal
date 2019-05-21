0a078c478d3252d3265a30fe300bcbc2fd2537bd
Niggarilomon <losgamers123456@gmail.com>
2019-05-21 09:45:41 -0300

    Noticias concluidas, faltas os stilos, achei izi

--- src/www/app/spa.js
+++ src/www/app/spa.js
@@ -19,12 +19,19 @@ app1.config(['$routeProvider', '$locationProvider', ($routeProvider, $locationPr
       templateUrl: 'app/views/projetos-grupos.html',
       controller: 'MainCtrl',
     })
+    .when('/:date/:post', {
+      templateUrl: (params) => { return `/blog/getPost/${params.date}/${params.post}`; },
+      controller: 'AllCtrl',
+    })
     .otherwise({ redirectTo: '/' });
 }]);
 
 app1.controller('MainCtrl', ['$rootScope', '$location', ($rootScope, $location) => {
   // eslint-disable-next-line no-param-reassign
   $rootScope.activetab = $location.path();
+}]);
+app1.controller('AllCtrl', ['$rootScope', '$routeParams', '$location', ($rootScope, $routeParams, $location) => {
+  $rootScope.activetab = $location.path() + $routeParams.date + $routeParams.post;
 }]);
 
 
