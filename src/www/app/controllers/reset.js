774e536a89d58666dc27c1235d147de004614b34
Niggarilomon <losgamers123456@gmail.com>
2019-08-14 11:48:25 -0300

    Reset Password e template de email reformado

--- src/www/app/controllers/reset.js
+++ src/www/app/controllers/reset.js
@@ -1,52 +1,37 @@
 //
 // Este script contém os controladores para modificações da senha
 //
-var app = angular.module('app', []); 
+const app = angular.module('app', []);
 
 // ====================== Controller reset password =================================
-app.controller('reset', ['$scope', '$http', '$location',($scope, $http, $location) => {
-    $scope.reset = () => {
-        data = $location.search();
-        return data;
-    };
- 
-    $scope.submit = (data) => {
-        password = $scope.password;
+app.controller('reset', ['$scope', '$http', '$location', ($scope, $http, $location) => {
+  $scope.reset = () => {
+    const data = $location.search();
+    return data;
+  };
 
-        if(password == undefined){
-            alert('Há compos vazios!');
-            return 0;
-        }
+  $scope.submit = (data) => {
+    const { password, Rpassword } = $scope;
 
-        json = {email:data.email,token:data.token,password:data.password};
-        
-        $http.post('/auth/reset_password', json)
-        .success(function(response) {
-            console.log(response);
-        })
-        .error(function(response){
-            console.log(response);
-        });
-
+    if (password === undefined || Rpassword === undefined) {
+      alert('Há compos vazios!');
+      return 0;
     }
 
- }]);
-// ====================== Controlador forget password ================
-app.controller('forgot', ['$scope', '$http',($scope, $http) => {
-    $scope.forgot = () => {
-        email = {email};
-        
-        $http.post('/auth/forgot_password', email)
-        .success((response)=> {
-            console.log(response);
-        })
-        .error((response) => {
-            console.log(response);
-        })
-
-
+    if (password !== Rpassword) {
+      alert('Senhas não batem');
+      return 0;
     }
+
+    const json = { email: data.email, token: data.token, password };
+
+    $http.post('/auth/reset_password', json)
+      .success((response) => {
+        console.log(response);
+        window.location.href = '/';
+      })
+      .error((response)=> {
+        console.log(response);
+      });
+  };
 }]);
- 
- 
- 
